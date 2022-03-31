8fa04e6ab735642148fdfdb96d61a19bbeaa0077
yashar <n.yashar@mail.ru>
2022-03-31 16:12:32 +0300

    done

--- app/src/main/java/iam/thevoid/epic/timeapp/MainActivity.kt
+++ app/src/main/java/iam/thevoid/epic/timeapp/MainActivity.kt
@@ -20,6 +20,7 @@ import io.reactivex.rxjava3.annotations.NonNull
 import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.internal.util.BackpressureHelper.add
 import io.reactivex.rxjava3.schedulers.Schedulers
 import io.reactivex.rxjava3.schedulers.Schedulers.io
@@ -65,9 +66,18 @@ class MainActivity : AppCompatActivity() {
     // Секундомер
     private lateinit var stopwatchText: TextView
     private lateinit var stopwatchMillisText: TextView
-    private lateinit var stopwatchStartButton: EditText
-    private lateinit var stopwatchEndButton: EditText
+    private lateinit var stopwatchStartButton: Button
+    private lateinit var stopwatchEndButton: Button
 
+    private var disposableStopwatch: Disposable? = null
+    private var cur_ms: Long = 0L
+
+    // состояние обратного отсчета
+    enum class StopwatchState {
+        STOPPED, PAUSED, RUNNING
+    }
+
+    private var stopwatchState: StopwatchState = StopwatchState.STOPPED
 
     @SuppressLint("RestrictedApi")
     @RequiresApi(Build.VERSION_CODES.O)
@@ -91,57 +101,165 @@ class MainActivity : AppCompatActivity() {
         countdownText = findViewById(R.id.countdownText)
         countdownSecondsEditText = findViewById(R.id.countdownEditText)
         countdownStartButton = findViewById(R.id.countdownStartButton)
+        var disposableCountdownTimer: Disposable? = null
 
+        fun disposeCountdown() {
+            disposableCountdownTimer?.dispose()
+            disposableCountdownTimer = null
+        }
 
-            countdownStartButton.setOnClickListener {
-                val total_time:Long = countdownSecondsEditText.text.toString().toLong()
-                timer(total_time)
-                    .unsubscribeOn(Schedulers.io())
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe { it ->
-                        countdownText.text = it
-                    }
-            }
+        countdownStartButton.setOnClickListener {
+            disposeCountdown()
+            var total_time: Long = countdownSecondsEditText.text.toString().toLong()
+            timer(total_time)
+            disposableCountdownTimer = timer(total_time)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { it ->
+                    countdownText.text = it
+                }
+        }
 
-/*
+//секундомер
+
         stopwatchText = findViewById(R.id.stopwatchText)
         stopwatchMillisText = findViewById(R.id.stopwatchMillisText)
         stopwatchStartButton = findViewById(R.id.stopwatchStartButton)
         stopwatchEndButton = findViewById(R.id.stopwatchEndButton)
 
-*/
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onStop() {
+        super.onStop()
+        if (stopwatchState == StopwatchState.RUNNING) {
+            disposeStopwatch()
+            stopwatchState = StopwatchState.PAUSED
+            stopwatchStartButton.text = "Resume"
+            stopwatchEndButton.text = "Stop"
         }
-}
+    }
+
+    private fun disposeStopwatch() {
+        disposableStopwatch?.dispose()
+        disposableStopwatch = null
+    }
+
+    private fun setStopwatchFirstTime() {
+        val texts = stopwatch_format(0L)
+        stopwatchText.text = texts.first
+        stopwatchMillisText.text = texts.second
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onStart() {
+        super.onStart()
+
+        //секундомер
+        stopwatchStartButton.setOnClickListener {
+            when (stopwatchState) {
+                StopwatchState.PAUSED, StopwatchState.STOPPED -> {
+                    stopwatchState = StopwatchState.RUNNING
+                    stopwatchStartButton.text = "Start"
+                    stopwatchEndButton.text = "Pause"
+                }
+                StopwatchState.RUNNING -> {
+                    cur_ms = 0L
+                }
+            }
+
+            disposeStopwatch()
+            disposableStopwatch = stopwatchtimer(cur_ms)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { item ->
+                    stopwatchText.text = item.first
+                    stopwatchMillisText.text = item.second
+                    cur_ms = item.third
+                }
+        }
+
+        stopwatchEndButton.setOnClickListener {
+            disposeStopwatch()
+            when (stopwatchState) {
+                StopwatchState.RUNNING -> {
+                    stopwatchState = StopwatchState.PAUSED
+                    stopwatchStartButton.text = "Resume"
+                    stopwatchEndButton.text = "Stop"
+                }
+                StopwatchState.PAUSED -> {
+                    stopwatchState = StopwatchState.STOPPED
+                   cur_ms = 0L
+                    stopwatchStartButton.text = "Start"
+                    stopwatchEndButton.text = "Pause"
+
+                    setStopwatchFirstTime()
+                }
+                else -> {
+                    Log.d(LOG_TAG, stopwatchState.toString())
+                }
+            }
+        }
+
+    }
+
 
 //часы
 
     @RequiresApi(Build.VERSION_CODES.O)
-    fun Clock_fun():Observable<String> {
+    fun Clock_fun(): Observable<String> {
         val format_time = DateTimeFormatter.ofPattern("HH:mm:ss")
         fun time(): String? {
             return LocalDateTime.now().format(format_time)
         }
-        return Observable.interval(1,TimeUnit.SECONDS).map { time() }
-        }
+        return Observable.interval(1, TimeUnit.SECONDS).map { time() }
+    }
 
 
 //таймер
 
+    var DisposibleTimer: Disposable? = null
+
     @RequiresApi(Build.VERSION_CODES.O)
-    fun timer(total_time:Long): @NonNull Observable<String> {
-        fun countdownTime(total_time: Long, t:Long): String? {
-           return DateUtils.formatElapsedTime(total_time - t)
+    fun timer(total_time: Long): @NonNull Observable<String> {
+        fun countdownTime(total_time: Long, t: Long): String? {
+            return DateUtils.formatElapsedTime(total_time - t)
         }
-        return Observable.interval(1,TimeUnit.SECONDS)
-            .takeWhile { it <=total_time.toLong() }
-            .map { t -> countdownTime(total_time.toLong(),t)}
-            }
+        return Observable.interval(1, TimeUnit.SECONDS)
+
+            .takeWhile { it <= total_time.toLong() }
+            .map { t -> countdownTime(total_time.toLong(), t) }
+    }
 
 
 //секундомер
 
+    // Секундомер - форматирование
+    private fun stopwatch_format(millisec: Long): Triple<String, String, Long> {
+        val hours = TimeUnit.MILLISECONDS.toHours(millisec)
+        val mins = TimeUnit.MILLISECONDS.toMinutes(millisec) - TimeUnit.HOURS.toMinutes(
+            TimeUnit.MILLISECONDS.toHours(millisec)
+        )
+        val sec = TimeUnit.MILLISECONDS.toSeconds(millisec) - TimeUnit.MINUTES.toSeconds(
+            TimeUnit.MILLISECONDS.toMinutes(millisec)
+        )
+        val millisecend = millisec - TimeUnit.SECONDS.toMillis(TimeUnit.MILLISECONDS.toSeconds(millisec))
 
+        val hms = String.format("%02d:%02d:%02d", hours, mins, sec)
+        val ms = String.format("%02d", millisecend)
+        return Triple(hms, ms, millisec)
+    }
+
+    // Секундомер - счетчик
+    private fun stopwatchtimer(cur_ms: Long): Observable<Triple<String, String, Long>> {
+        return Observable.interval(1, TimeUnit.MILLISECONDS)
+            .map { t -> stopwatch_format(t * 1 + cur_ms) }
+    }
+
+    companion object {
+        private val LOG_TAG = MainActivity::class.simpleName
+    }
+}
 
 
 
